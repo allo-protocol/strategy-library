3d2b42455bab48dfb440d228374d701027f1173c
Jaxcoder <codenamejason@outlook.com>
2023-06-16 13:44:07 -0400

    adding specs and base examples

--- src/distribution/simple-voting/SimpleVoting.sol
+++ src/distribution/simple-voting/SimpleVoting.sol
@@ -1,6 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.19;
 
-import {IDistributionStrategy} from "allo-v2/contracts/core/interfaces/IDistributionStrategy.sol";
+import {IDistributionStrategy} from "../../../lib/allo-v2/contracts/core/interfaces/IDistributionStrategy.sol";
 
-contract SimpleVoting is IDistributionStrategy {}
+contract SimpleVoting is IDistributionStrategy {
+    // State variables
+    address public strategyOwner;
+    
+    constructor(address _strategyOwner) {
+        strategyOwner = _strategyOwner;
+    }
+
+    function owner() public view override returns (address) {
+        return strategyOwner;
+    }
+
+    function activateDistribution(bytes memory _data) public override {
+        // Implement the function here, including decoding _data and activating distribution.
+    }
+
+    function claim(bytes memory _data) public override {
+        // Implement the function here, including decoding _data and claiming a payout.
+    }
+}

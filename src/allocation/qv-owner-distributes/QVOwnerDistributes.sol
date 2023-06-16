f8311d91b3b4f4f57fc93decdcaa414fab025b3d
Jaxcoder <codenamejason@outlook.com>
2023-06-16 14:57:49 -0400

    update placeholders

--- src/allocation/qv-owner-distributes/QVOwnerDistributes.sol
+++ src/allocation/qv-owner-distributes/QVOwnerDistributes.sol
@@ -1,25 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {IDistributionStrategy} from "../../../lib/allo-v2/contracts/core/interfaces/IDistributionStrategy.sol";
+import {IAllocationStrategy} from "../../../lib/allo-v2/contracts/core/interfaces/IAllocationStrategy.sol";
 
-contract QVOwnerDistributes is IDistributionStrategy {
+contract QVOwnerDistributes is IAllocationStrategy {
     // State variables
     address public strategyOwner;
-    
-    constructor(address _strategyOwner) {
-        strategyOwner = _strategyOwner;
+
+    // Constructor
+    constructor() {
+        strategyOwner = msg.sender;
     }
 
     function owner() public view override returns (address) {
         return strategyOwner;
     }
 
-    function activateDistribution(bytes memory _data) public override {
-        // Implement the function here, including decoding _data and activating distribution.
+    function applyToPool(
+        bytes memory _data
+    ) external payable override returns (bytes memory) {
+        // Implement the function here, including decoding _data and storing the applicant.
+        // Return some bytes data.
+        return _data;
     }
 
-    function claim(bytes memory _data) public override {
-        // Implement the function here, including decoding _data and claiming a payout.
+    function allocate(
+        bytes memory _data
+    ) external payable override returns (uint) {
+        // Implement the function here, including decoding _data and performing necessary actions.
+        // Return the number of votes cast. For now, return a dummy value.
+        (, uint amount) = abi.decode(_data, (address, uint));
+
+        return amount;
+    }
+
+    function generatePayouts()
+        external
+        payable
+        override
+        returns (bytes memory)
+    {
+        // Implement the function here, including generating payouts.
+        // Return some bytes data. For now, return an empty bytes array.
+        return "";
     }
 }
